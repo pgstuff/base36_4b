da31a6b12896c0f5108b804c56ce9e6ab5343b1c
User 1 <user1@localhost.localdomain>
2016-02-11 03:57:06 -0500

    Keep extension functions together when sorted with other functions.

--- sql/base36_4b.sql
+++ sql/base36_4b.sql
@@ -47,7 +47,7 @@ LANGUAGE C IMMUTABLE STRICT;
 -- to/from text conversion
 CREATE OR REPLACE FUNCTION base36_4b_to_text(base36_4b) RETURNS text AS '$libdir/base36_4b'
 LANGUAGE C IMMUTABLE STRICT;
-CREATE OR REPLACE FUNCTION text_to_base36_4b(text) RETURNS base36_4b AS '$libdir/base36_4b'
+CREATE OR REPLACE FUNCTION base36_4b_from_text(text) RETURNS base36_4b AS '$libdir/base36_4b'
 LANGUAGE C IMMUTABLE STRICT;
 
 -- operators
@@ -116,4 +116,4 @@ CREATE OPERATOR CLASS base36_4b_ops DEFAULT FOR TYPE base36_4b USING btree AS
 	FUNCTION 1 base36_4b_cmp(base36_4b, base36_4b);
 -- cast from/to text
 CREATE CAST (base36_4b AS text) WITH FUNCTION base36_4b_to_text(base36_4b) AS ASSIGNMENT;
-CREATE CAST (text AS base36_4b) WITH FUNCTION text_to_base36_4b(text) AS ASSIGNMENT;
+CREATE CAST (text AS base36_4b) WITH FUNCTION base36_4b_from_text(text) AS ASSIGNMENT;
